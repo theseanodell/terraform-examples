15fc7cac05576712cd6cd08f7e7f92b8c76f861a
Sean O'Dell <sean.odell@cloudychance.dev>
2021-04-12 20:00:28 +0000

    update variables

--- aws/complete-vpc/variables.tf
+++ aws/complete-vpc/variables.tf
@@ -3,3 +3,35 @@ variable "region" {
   type        = string
   default     = "us-east-2"
 }
+variable "vpc-name" {
+  description = "AWS VPC Name"
+  type        = string
+}
+variable "customer" {
+  description = "Customer"
+  type        = string
+}
+variable "product" {
+  description = "Product"
+  type        = string
+}
+variable "team" {
+  description = "Team"
+  type        = string
+}
+variable "owner" {
+  description = "Owner"
+  type        = string
+}
+variable "environment" {
+  description = "Environment"
+  type        = string
+}
+variable "organization" {
+  description = "Organization"
+  type        = string
+}
+variable "cost-center" {
+  description = "Cost Center"
+  type        = string
+}
